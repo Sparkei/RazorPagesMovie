6ebe270d1c39d4a3a81b2ccabaa02e18a185e958
Mark O'reilly <markoreilly92@gmail.com>
2020-10-13 18:13:12 +0100

    Initial commit

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovie.Data;
+using System;
+using System.Linq;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Man v Mattress",
+                        ReleaseDate = DateTime.Parse("2020-3-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Coughing Bob",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Drama",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Andy's Fifa Nightmare",
+                        ReleaseDate = DateTime.Parse("2004-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Seed Data",
+                        ReleaseDate = DateTime.Parse("2020-10-13"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
